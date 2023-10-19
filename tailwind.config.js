e0b8a5ac4d8de8c27202326a0e517925da931082
ecoglito <ecoglito@usc.edu>
2023-10-18 18:41:21 -0700

    add font colors to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-extend: {
+    extend: {
       fontFamily: {
-        manrope: ['Manrope', 'sans-serif'],
+        'manrope': ['Manrope', 'sans-serif']
       },
-    },
+      colors: {
+        'modal-background': '#000000',
+        'component-background': '#111111',
+        'border-color': '#2a2a2e',
+        'accent-color': '#c4c4c4',
+        'unselected-text': '#746F6F',
+        'shadow-element': '#242424',
+        'success-green': '#57FFC0',
+        'loading-yellow': '#FECE51',
+        'failure-red': '#DB6666'
+      },
+      borderWidth: {
+        '1': '1px'
+      }
+    }
   },
   plugins: [],
 };
