6fc0cce8d86f1a3ba44ed898ecb4a84f4ba15765
ecoglito <ecoglito@usc.edu>
2023-10-20 11:38:59 -0700

    fix props

--- src/components/TransferWidget.tsx
+++ src/components/TransferWidget.tsx
@@ -6,40 +6,8 @@ export interface TransferWidgetProps {
 }
 
 const TransferWidget: React.FC<TransferWidgetProps> = ({ actionbutton }) => {
-  const [buttonState, setButtonState] = useState('default');
-
-  const { buttonLabel, bgColor, textColor } = useMemo(() => {
-    let buttonLabel = '';
-    let bgColor = '';
-    let textColor = '';
-
-    switch(buttonState) {
-      case 'default':
-        buttonLabel = "Select Tokens";
-        bgColor = 'bg-component-bg';
-        textColor = 'text-black';
-        break;
-      case 'review bridge':
-        buttonLabel = 'Review Bridge';
-        bgColor = 'bg-success-green';
-        textColor = 'text-white';
-        break;
-      case 'error':
-        buttonLabel = 'Error';
-        bgColor = 'bg-failure-red';
-        textColor = 'text-white';
-        break;
-      // Add more cases as needed
-    }
-
-    return { buttonLabel, bgColor, textColor };
-  }, [buttonState]);
-
   return (
     <div>
-      <a onClick={() => setButtonState('review bridge')}>
-        <p>hi</p>
-      </a>
       <ActionButton {...actionbutton} />
     </div>
   )
