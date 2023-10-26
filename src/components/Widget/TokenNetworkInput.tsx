3d107440d2cc585c7dcce04163df56d6d1f062e3
ecoglito <ecoglito@usc.edu>
2023-10-25 17:47:58 -0700

    comments

--- src/components/Widget/TokenNetworkInput.tsx
+++ src/components/Widget/TokenNetworkInput.tsx
@@ -37,31 +37,31 @@ export const TokenNetworkInput: FunctionComponent<TokenNetworkInputProps> = ({
         <div className="flex flex-col">
           {status === 'default'
             ? (
-            <div className="flex flex-row gap-2 items-center">
-              <a href="#" className="text-unselected-text whitespace-nowrap" onClick={onAnchorClick}>
-                Select {direction} chain and token
-              </a>
-              <div className = "w-[53px] h-[50px]">
-                <svg width="53" height="50" viewBox="0 0 64 59" fill="none" xmlns="http://www.w3.org/2000/svg" >
+              <div className="flex flex-row gap-2 items-center">
+                <a href="#" className="text-unselected-text whitespace-nowrap" onClick={onAnchorClick}>
+                  Select {direction} chain and token
+                </a>
+                <div className="w-[53px] h-[50px]">
+                  <svg width="53" height="50" viewBox="0 0 64 59" fill="none" xmlns="http://www.w3.org/2000/svg" >
                     <path fillRule="evenodd" clipRule="evenodd" d="M57.6155 36.7243C58.2024 34.4136 58.5143 31.9931 58.5143 29.4998C58.5143 13.3415 45.4154 0.242676 29.2571 0.242676C13.0989 0.242676 0 13.3415 0 29.4998C0 45.6581 13.0989 58.757 29.2571 58.757C34.4339 58.757 39.2966 57.4125 43.5152 55.0536C41.9733 53.0384 41.0571 50.519 41.0571 47.7855C41.0571 41.1739 46.4169 35.8141 53.0286 35.8141C54.6535 35.8141 56.2028 36.1378 57.6155 36.7243ZM57.3501 37.698C56.0239 37.1291 54.563 36.8141 53.0286 36.8141C46.9692 36.8141 42.0571 41.7262 42.0571 47.7855C42.0571 50.3359 42.9273 52.683 44.387 54.5462C50.5858 50.7936 55.2797 44.8047 57.3501 37.698Z" fill="#242424" />
                     <circle cx="53.0286" cy="47.7854" r="10.9714" fill="#242424" />
-                </svg>
+                  </svg>
+                </div>
               </div>
-            </div>
-              )
+            )
             : (
-            <div className="flex flex-row gap-1 justify-center items-center cursor-pointer" onClick={onAnchorClick}>
-              <div className="flex flex-col items-stretch h-full">
-                <p className="text-white font-manrope text-lg">
-                  {tokenName}
-                </p>
-                <p className="text-accent-color font-manrope text-sm">
-                  {chainName}
-                </p>
+              <div className="flex flex-row gap-1 justify-center items-center cursor-pointer" onClick={onAnchorClick}>
+                <div className="flex flex-col items-stretch h-full">
+                  <p className="text-white font-manrope text-lg">
+                    {tokenName}
+                  </p>
+                  <p className="text-accent-color font-manrope text-sm">
+                    {chainName}
+                  </p>
+                </div>
+                <TokenNetworkImage logo={tokenLogo} networkLogo={tokenNetwork} />
               </div>
-              <TokenNetworkImage logo={tokenLogo} networkLogo={tokenNetwork} />
-            </div>
-              )}
+            )}
         </div>
       </div>
 
--- src/components/Widget/TokenNetworkSelector.tsx
+++ src/components/Widget/TokenNetworkSelector.tsx
@@ -1,6 +1,6 @@
-import React, {FunctionComponent, useSelect} from 'react'
-import {DefaultTooltip} from '../Tooltips/DefaultTooltip'
-import {motion} from 'framer-motion'
+import React, { FunctionComponent, useSelect } from 'react'
+import { DefaultTooltip } from '../Tooltips/DefaultTooltip'
+import { motion } from 'framer-motion'
 
 export interface SupportedNetworkProps {
     network: 'ethereum' | 'polygon' | 'arbitrum' | 'optimism' | 'goerli' | 'mumbai' // chain id / name
@@ -29,35 +29,35 @@ export interface TokenNetworkSelectorProps {
     onClose?: () => void;
 }
 
-const NetworkSelector: FunctionComponent<SupportedNetworkProps> = ({ network, chainName, networkLogo}) => {
+const NetworkSelector: FunctionComponent<SupportedNetworkProps> = ({ network, chainName, networkLogo }) => {
     const Network = () => {
         return (
-            <DefaultTooltip label = {chainName} side = 'top'>
-                <img src = {networkLogo} className = 'w-12 h-12'/>
+            <DefaultTooltip label={chainName} side='top'>
+                <img src={networkLogo} className='w-12 h-12' />
             </DefaultTooltip>
         )
 
     }
     return (
-        <div className = 'flex flex-col w-full gap-3 bg-component-background py-3 px-2 rounded-lg border-border-color border-1'>
-             <div className = 'flex flex-row justify-between items-center w-full'>
+        <div className='flex flex-col w-full gap-3 bg-component-background py-3 px-2 rounded-lg border-border-color border-1'>
+            <div className='flex flex-row justify-between items-center w-full'>
                 <Network />
                 <Network />
                 <Network />
                 <Network />
-             </div>
-          
-             <p className="text-accent-color font-manrope font-medium text-md">Network: {chainName.charAt(0).toUpperCase() + chainName.slice(1)}</p>
+            </div>
+
+            <p className="text-accent-color font-manrope font-medium text-md">Network: {chainName.charAt(0).toUpperCase() + chainName.slice(1)}</p>
         </div>
     )
 }
 
-const SupportedToken: FunctionComponent<SupportedTokenProps> = ({tokenName, tokenDescription, tokenLogo, balance}) => {
+const SupportedToken: FunctionComponent<SupportedTokenProps> = ({ tokenName, tokenDescription, tokenLogo, balance }) => {
     return (
-        <div className = 'flex w-full flex-row justify-between items-center hover:bg-shadow-element cursor-pointer hover:rounded-lg'>
-            <div className = 'flex flex-row gap-1 items-center'>
-                <img className = 'w-12 h-12' src = {tokenLogo}/>
-                <div className = 'flex flex-col'>
+        <div className='flex w-full flex-row justify-between items-center hover:bg-shadow-element cursor-pointer hover:rounded-lg'>
+            <div className='flex flex-row gap-1 items-center'>
+                <img className='w-12 h-12' src={tokenLogo} />
+                <div className='flex flex-col'>
                     <p className="text-white font-manrope text-lg">
                         {tokenName}
                     </p>
@@ -73,11 +73,11 @@ const SupportedToken: FunctionComponent<SupportedTokenProps> = ({tokenName, toke
     )
 }
 
-const SupportedTokens: FunctionComponent<SupportedTokensProps> = ({network, token}) => {
+const SupportedTokens: FunctionComponent<SupportedTokensProps> = ({ network, token }) => {
     return (
-        <div className = 'flex flex-col gap-2'>
-            <input className = 'bg-component-background border-1 border-border-color py-4 rounded-lg px-2 text-white placeholder-unselected-text font-Manrope' placeholder="Search by name or address"/>
-            <div className = 'flex bg-component-background rounded-lg border-1 border-border-color px-2 py-4 flex-col w-full gap-3 max-h-[300px] overflow-y-auto'>
+        <div className='flex flex-col gap-2'>
+            <input className='bg-component-background border-1 border-border-color py-4 rounded-lg px-2 text-white placeholder-unselected-text font-Manrope' placeholder="Search by name or address" />
+            <div className='flex bg-component-background rounded-lg border-1 border-border-color px-2 py-4 flex-col w-full gap-3 max-h-[300px] overflow-y-auto'>
                 <SupportedToken {...token} />
                 <SupportedToken {...token} />
                 <SupportedToken {...token} />
@@ -90,58 +90,58 @@ const SupportedTokens: FunctionComponent<SupportedTokensProps> = ({network, toke
     );
 }
 
-const TokenNetworkSelector: FunctionComponent<TokenNetworkSelectorProps> = ({direction, network, tokens, onClose}) => {
+const TokenNetworkSelector: FunctionComponent<TokenNetworkSelectorProps> = ({ direction, network, tokens, onClose }) => {
     return (
         <motion.div
-        initial={{ opacity: .25, scale: 0.9 }}
-        animate={{ opacity: 1, scale: 1 }}
-        exit={{ opacity: .25, scale: 0.9, transition: { duration: 0.5 } }}
-        transition={{ duration: 0.5, type: 'spring', bounce: 0.3 }}
-      >
-        <div className = 'inline-flex flex-col py-5 px-6 gap-4 border-1 rounded-lg border-border-color bg-modal-background sm:w-[90vw] sm:min-w-[300px] max-w-[475px]'>
-            <div className = 'flex flex-col gap-2'>
-               
-                <motion.div
-                    initial={{ opacity: 0, scale: 0.9 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 0.5, type: 'spring', bounce: 0.3 }}
-                >
-                    <div className = 'flex flex-row justify-between items-center'>
-                        <p className="text-white font-manrope font-medium text-xl">Select {direction.charAt(0).toUpperCase() + direction.slice(1)} Network</p>
-                        <div className="p-2 hover:bg-shadow-element hover:rounded-lg cursor-pointer" onClick={onClose}>
-                            <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" clip-rule="evenodd" d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z" fill="white"/>
-                            </svg>
-                        </div>
-                    </div>
-                </motion.div>
-                
-                <motion.div
-                    initial={{ opacity: 0, scale: 0.9 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{delay: 0.3, duration: 0.5, type: 'spring', bounce: 0.3 }}
-                >
-                    <NetworkSelector {...network}/>
-                </motion.div>
-            </div>
+            initial={{ opacity: .25, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: .25, scale: 0.9, transition: { duration: 0.5 } }}
+            transition={{ duration: 0.5, type: 'spring', bounce: 0.3 }}
+        >
+            <div className='inline-flex flex-col py-5 px-6 gap-4 border-1 rounded-lg border-border-color bg-modal-background sm:w-[90vw] sm:min-w-[300px] max-w-[475px]'>
+                <div className='flex flex-col gap-2'>
 
-            <div className = 'flex flex-col gap-2'>
-                <motion.div
+                    <motion.div
                         initial={{ opacity: 0, scale: 0.9 }}
                         animate={{ opacity: 1, scale: 1 }}
                         transition={{ duration: 0.5, type: 'spring', bounce: 0.3 }}
                     >
-                <p className="text-white font-manrope font-medium text-xl">Select {direction.charAt(0).toUpperCase() + direction.slice(1)} Token</p>
-                </motion.div>
+                        <div className='flex flex-row justify-between items-center'>
+                            <p className="text-white font-manrope font-medium text-xl">Select {direction.charAt(0).toUpperCase() + direction.slice(1)} Network</p>
+                            <div className="p-2 hover:bg-shadow-element hover:rounded-lg cursor-pointer" onClick={onClose}>
+                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z" fill="white" />
+                                </svg>
+                            </div>
+                        </div>
+                    </motion.div>
+
                     <motion.div
                         initial={{ opacity: 0, scale: 0.9 }}
                         animate={{ opacity: 1, scale: 1 }}
-                        transition={{delay: 0.3, duration: 0.5, type: 'spring', bounce: 0.3 }}
+                        transition={{ delay: 0.3, duration: 0.5, type: 'spring', bounce: 0.3 }}
                     >
-                    <SupportedTokens {...tokens} />
-                </motion.div>
+                        <NetworkSelector {...network} />
+                    </motion.div>
+                </div>
+
+                <div className='flex flex-col gap-2'>
+                    <motion.div
+                        initial={{ opacity: 0, scale: 0.9 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{ duration: 0.5, type: 'spring', bounce: 0.3 }}
+                    >
+                        <p className="text-white font-manrope font-medium text-xl">Select {direction.charAt(0).toUpperCase() + direction.slice(1)} Token</p>
+                    </motion.div>
+                    <motion.div
+                        initial={{ opacity: 0, scale: 0.9 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{ delay: 0.3, duration: 0.5, type: 'spring', bounce: 0.3 }}
+                    >
+                        <SupportedTokens {...tokens} />
+                    </motion.div>
+                </div>
             </div>
-        </div>
         </motion.div>
     )
 }
